aacc33a1fc87190b6dd73c86ebf5301223eeeca3
Akshayalekshmi <akshayamakairam@gmail.com>
2022-07-23 15:30:25 +0530

    storing new datas

--- app.js
+++ app.js
@@ -1,16 +1,47 @@
 const Express=require("express")
 const Bodyparser=require("body-parser")
-const Mongoose=require("mongoose")
+const Mongoose=require("mongoose")  
 const Cors=require("cors")
+const {courseModel}=require("./coursemodel")
+
 
 const app=Express()
-app.use(Express.json({extended:false}))
+app.use(Bodyparser.urlencoded({extended:true}))
+app.use(Bodyparser.json())
+app.use(Cors())
+Mongoose.connect("mongodb+srv://akshayalekshmi:akshaya0810@cluster0.olkq9kj.mongodb.net/CollegeDB?retryWrites=true&w=majority")
 
-app.get("/addcourse",(req,res)=>{
-    res.send("Welcome to Addcourse")
+app.post("/addcourse",async(req,res)=>{
+    const data=req.body
+    const ob=new courseModel(data)
+    ob.save(
+        (error,data)=>{
+            if(error)
+            {
+                res.send("error occured")
+
+            }
+            else{
+                res.send(data)
+            }
+        }
+    )
+
+    
 })
-app.get("/viewcourse",(req,res)=>{
-    res.send("Welcome to Viewcourse")
+app.get("/viewcourse",async(req,res)=>{
+    courseModel.find(
+        (error,data)=>{
+            if(error)
+            {
+                res.send(error)
+            }
+            else{
+                res.send(data)
+            }
+        }
+    )
+    
 })
 app.get("/searchcourse",(req,res)=>{
     res.send("Welcome to Searchcourse")
---
+++ coursemodel.js
@@ -0,0 +1,21 @@
+const Mongoose=require("mongoose")
+
+const courseModel=Mongoose.model("courses",Mongoose.Schema({
+
+    courseTitle:String,
+    courseDescription:String,
+    courseDuration:String,
+    courseVenue:String,
+    courseDate:String
+
+}))
+module.exports={courseModel}
+
+
+
+
+
+
+
+
+
